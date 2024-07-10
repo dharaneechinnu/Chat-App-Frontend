4442f09b9d560388a284d17ede0df78866d6dd45
dharaneedharan <117936700+dharaneechinnu@users.noreply.github.com>
2024-07-10 14:07:07 +0530

    ff

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AuthContextProvider } from './Context/AuthContext.jsx';
 import { ChatContextProvider } from './Context/ChatContent.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthContextProvider>
         {/* Assuming user information is required for ChatContextProvider, wrap it inside AuthContextProvider */}
         <AuthContextProvider>
@@ -17,6 +17,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           </ChatContextProvider>
         </AuthContextProvider>
       </AuthContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
